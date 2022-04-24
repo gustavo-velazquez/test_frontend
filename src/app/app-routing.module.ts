b03da9eaee606e86f95e0494adf5e4e6e206b19c
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-24 19:24:24 -0300

    se agrego estructura html a post y su ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
 import { NgModule } from '@angular/core';
 import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 const routes: Routes = [
   {path: '',component: PostsComponent},
+  {path: 'post/:id', component: PostComponent},
   {path: '**', component: Error404Component}
- 
 ];
 
 @NgModule({
