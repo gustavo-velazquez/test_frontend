b2d75508271a7f89562b26441c6ba4ed63ef12b0
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-24 17:23:54 -0300

    creacion de componentes 404,post y posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Error404Component } from './components/error404/error404.component';
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
