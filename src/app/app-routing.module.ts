cae38a4599a8e93e54a9460f93bfa19ac65ebdbe
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-24 19:02:41 -0300

    se agrego tabla a posts y se arreglo rutas correspondientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './components/error404/error404.component';
 import { NgModule } from '@angular/core';
+import { PostsComponent } from './components/posts/posts.component';
 
 const routes: Routes = [
-  {path: '', component: Error404Component}
+  {path: '',component: PostsComponent},
+  {path: '**', component: Error404Component}
+ 
 ];
 
 @NgModule({
