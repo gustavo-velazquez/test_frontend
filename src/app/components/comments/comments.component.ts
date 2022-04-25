23ba67baab8cc6cb6968227b433f39e50c2ef4b6
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-25 14:57:48 -0300

    Se obtienen comentarios de cada post y cambios de estilo

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { comment } from 'src/app/models/comment.model';
+import { PostService } from 'src/app/servervices/post.service';
 
 @Component({
   selector: 'app-comments',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
+  @Input() postId !: number;
 
-  constructor() { }
+  comments : comment[] = [];
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.getcomments();
+  }
+
+  getcomments(){
+    this.postService.getComment(this.postId)
+    .subscribe((response: comment[]) => this.comments = response )
   }
 
 }
