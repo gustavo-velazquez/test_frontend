4f9b0c99dd3595cedd7c6c66dea493719419d726
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-25 20:25:16 -0300

    se agrego guardado en local storage de comment

--- src/app/servervices/post.service.ts
+++ src/app/servervices/post.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Post } from '../models/post.model';
-import { comment } from '../models/comment.model';
+import { Comment } from '../models/comment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+
+  comment : Comment | undefined;
 
   constructor(private http: HttpClient) { }
 
@@ -21,9 +23,22 @@ export class PostService {
     return this.http.get<Post>(url).pipe(map((data: Post) => data));
   }
 
-  public getComment(id: number): Observable<comment[]>{
+  public getComment(id: number): Observable<Comment[]>{
     const url:string = `https://jsonplaceholder.typicode.com/posts/${id}/comments`;
-    return this.http.get<comment[]>(url).pipe(map((data: comment[]) => data));
+    return this.http.get<Comment[]>(url).pipe(map((data: Comment[]) => data));
+  }
+
+  addComment(comment: Comment) {
+    this.comment = comment
+    this.saveLocalStorage();
+  }
+
+  public saveLocalStorage(){
+    localStorage.setItem('comment',JSON.stringify(this.comment));
+  }
+
+  public getCommentLocalStorage(){
+    return (localStorage.getItem('comment'))?  JSON.parse(localStorage.getItem('comment')!): this.comment ;
   }
 
 }
