7cf07cd38c40ef03d63ff9c9a424fbdd34c517c0
Gustavo Velazquez <gustavovelazquez1992@gmail.com>
2022-04-25 00:17:14 -0300

    se creo lista dinamica y se agrego paginacion con material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Error404Component } from './components/error404/error404.component';
 import { PostComponent } from './components/post/post.component';
 import { PostsComponent } from './components/posts/posts.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { PostsComponent } from './components/posts/posts.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Post } from 'src/app/models/post.model';
+import { PostService } from 'src/app/servervices/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+  public listPost: Post[] = [];
+  public pageSize:number = 5;
+  public starIndex:number = 0;
+  public endIndex:number = 5;
 
-  constructor() { }
+  constructor(private post:PostService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  public getData(){
+    this.post.get().subscribe((response : Post[])=> {
+      this.listPost = response;
+    })
+  }
+
+  public onPageChange(event: PageEvent){
+    this.starIndex = event.pageIndex * event.pageSize;
+    this.endIndex = this.starIndex + event.pageSize;
+   
   }
 
 }
